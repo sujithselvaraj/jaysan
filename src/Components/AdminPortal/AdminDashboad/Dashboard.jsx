fa0a76a854da70fd2a9c8c0cf6c7cc4fe84aad86
Nisanth2004 <nisanthjothi2004@gmail.com>
2025-03-22 17:19:16 +0530

    Add Product,Carreer

---
+++ src/Components/AdminPortal/AdminDashboad/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AdminNavBar from '../AdminNavBar/AdminNavBar'
+import Footer from '../../Footer/Footer'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <AdminNavBar/>
+
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default Dashboard
